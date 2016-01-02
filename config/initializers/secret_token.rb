4cbe5231b0c1be1fd6465f482f9c6d366d5b1336
Jaime M <j7isaac@gmail.com>
2016-01-02 04:50:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teamprojectquiz::Application.config.secret_key_base = '1f6d885400646b59e949a4ff84712746c35edecb149d108193ed899409336eafafd15c30a1b5017e96ac4f223fe4ff0cf043c1812a5d71b1e7f47e3cb99bd370'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Teamprojectquiz::Application.config.session_store :cookie_store, key: '_teamprojectquiz_session'
